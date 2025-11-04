51ccc63cfe400b6ed114c6ed937166d356d21a0e
victor-kakiuti-dev <victorkaki@hotmail.com>
2025-11-04 12:01:37 -0300

    implementando e ligando o banco de dados à aplicação

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+ 
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    article = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from utils.blog.factory import make_article
+from .models import Article
+
 
 def home(request):
+    articles = Article.objects.filter(
+        is_published=True,
+    ).order_by('-id')
     return render(request, 'blog/pages/home.html', context={
-        'article': [make_article() for _ in range(10)]
+        'article': articles,
     })
 
 def article(request, id):
+    article = Article.objects.filter(
+        is_published=True,
+    ).order_by('-id').first()
     return render(request, 'blog/pages/article_view.html', context={
-            'articles': make_article(),
+            'articles': article,
             'is_detail_page': True,
     })
 
-# Create your views here.
+
